63a74a1edf0ba2c2ca783fef6a96e1d3ed01b87b
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-24 11:20:23 -0400

    Initial commit

---
+++ src/HackathonApp.java
@@ -0,0 +1,21 @@
+import java.awt.Component;
+import java.awt.Menu;
+
+import javax.swing.JFrame;
+
+public class HackathonApp extends JFrame {
+	public HackathonApp(){
+	super("Hack-A-Thon Application");
+	Menu menu = new Menu();
+//	this.add(menu);
+	this.setVisible(true);
+	this.setSize(800,600);
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.setResizable(false);
+	}
+	
+	public static void main(String[] args){
+		new HackathonApp();
+	}
+
+}
---
+++ src/Menu.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+
+public class Menu extends JPanel{
+  public Menu(){
+    
+    super();
+  }
+}
