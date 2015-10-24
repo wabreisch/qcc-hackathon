4fc2c4acc984639030cf11ea6f2a5cb6c5914ecf
brian <brianrobgill@gmail.com>
2015-10-24 15:37:47 -0400

    Change Career Files for better code standards

--- src/Career.java
+++ src/Career.java
@@ -1,39 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
-
 //A Basic data class to represent the viable careers a user may be interested in..
 //By Brian Gill for team Wishbay, October 24 2015 Hackathon @ Quinnipiac University.
-
 public class Career {
-	
 	String name;
 	String description;
 	ArrayList<String> majors;
 	double salary;
-	
 	public Career(){
 		this.name = "nada";
 		this.salary = 0;
 		this.description = "nothing here";
 		this.majors = new ArrayList<String>();
 	}
-	
 	public Career(String name, double salary, String description){
 		this.name = name;
 		this.salary = salary;
 		this.description = description;
 		this.majors = new ArrayList<String>();
 	}
-
 	public ArrayList<String> getMajors(){
 		return majors;
 	}
-	
 	public void addMajor(String major){
 		majors.add(major);
 	} 
-	
 	public Boolean hasMajor(String major){
 		if (majors.contains(major)){
 			return true;
@@ -43,15 +35,11 @@ public class Career {
 	
 	public String getDescription(){
 		return this.description;
-	}
-	
-	
+	}	
 	public String getName(){
 		return name;
 	}
-	
 	public double getSalary(){
 		return salary;
 	}
-
 }
--- src/CareerTester.java
+++ src/CareerTester.java
@@ -1,23 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
-
 //A Basic Tester Class For the Career Manager to see if data is being used properly.
 //By Brian Gill for team Wishbay, October 24 2015 Hackathon @ Quinnipiac University.
-
 public class CareerTester {
-	
-	
 	static String major = "software";
-	static double debt = 1000000;
+	static double debt = 10000;
 	static CareerManager myManager;
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		myManager = new CareerManager();
-		System.out.println("Successfully Made the manager");
-		
+		System.out.println("Successfully Made the manager");	
 		System.out.println(myManager.debtToYears(major, debt));
 	}
-
 }
